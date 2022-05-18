4ad08c14f0c63b8a5abfd26b24ca5d5874059d52
TENSHI101 <48865498+garciarodrigue@users.noreply.github.com>
2022-05-17 23:43:25 -0600

    Add files via upload
    Demo

---
+++ Proof.py
@@ -0,0 +1,29 @@
+import os
+os.system("pkg install -y ttyrec")
+os.system("clear")
+banner = """         
+        <Tenshi> ┈┈┈╲╲╲╲╲╲┈┈┈┈ </Tenshi> 
+                 ┈┈╱╱╲╲╲╲╲╲┈┈┈┈┈ 
+                 ┈┈╱╱▏◤┃◥▕╲┈┈┈┈┈ 
+                 ┈┈╰┓▏╯╰╮▕╯┈┈┈┈┈ 
+                 ┈┈┈┃╭╯╰╯▕┈┈┈┈┈┈ 
+                 ┈┈┈┃┊┈╭╮▕┈┈┈┈┈┈ 
+                 ┈┈╱▔╲▁▁▁╱╲┈┈┈┈┈ 
+                 ▔▔╲╱▔╲▉╱╲╱▔▔▔╲┈
+"""
+print(banner)
+print("Hola soy el Profe")
+input()
+os.system("clear")
+print(banner)
+print(""" Te esnseñare a manejar la terminal como profecional\n 
+                     De esta manera""")
+os.system("ttyplay -s 2 ejemplo.rec")
+os.system("clear")
+print(banner)
+print("Quieres continuar como mi alumno?")
+
+
+
+  
+
